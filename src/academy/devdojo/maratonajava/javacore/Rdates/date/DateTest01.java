2b87514a5a1abc64532d60e954f2534d5764251a
Leonardo Santana da Silva <lsantanasilva98@gmail.com>
2025-08-19 16:41:38 -0300

    Adicionando classe date

--- src/academy/devdojo/maratonajava/javacore/Rdates/date/DateTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdates/date/DateTest01.java
@@ -1,5 +1,11 @@
 package academy.devdojo.maratonajava.javacore.Rdates.date;
 
-public class DateTest01 {
+import java.util.Date;
 
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1256325632563L); // milisegundos
+        date.setTime(date.getTime() + 3_600_000L);
+        System.out.println(date);
+    }
 }
