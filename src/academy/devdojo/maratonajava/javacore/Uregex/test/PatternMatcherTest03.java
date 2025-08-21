7b51b8a0066b2c76a0452b440cc7fbfef47d5127
Leonardo Santana da Silva <lsantanasilva98@gmail.com>
2025-08-21 15:31:56 -0300

    Refactor: adiciona aulas sobre regex

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d = Todos os digitos
+        // \D = Tudo que nao for digito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-zA-Z, digitos, _
+        //\W = Tudo que nao for incluso no \w
+
+        // String regex = "[a-zA-Z]";
+
+        String regex = "0[xX][0-9a-fA-F]";
+        String texto = "12 0X 0x 0xFFABC 0x109 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto   "+texto);
+        System.out.println("Indice: 0123456789");
+        System.out.println("Regex: "+regex);
+
+        System.out.println("Posições encontradas");
+        while(matcher.find()){
+            System.out.print(matcher.start()+" "+matcher.group()+"\n");
+        }
+
+
+    }
+}
