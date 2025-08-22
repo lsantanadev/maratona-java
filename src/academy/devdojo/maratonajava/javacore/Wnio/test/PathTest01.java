68e80ebb3eb65f381835c75a16399c261f9cc5d7
Leonardo Santana da Silva <lsantanasilva98@gmail.com>
2025-08-21 21:24:06 -0300

    feat: adicionar exemplos de uso do Java NIO (Path, Paths, Files, normalize)

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest01 {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("/home/lssdev/projects/maratona-java/file.txt");
+        System.out.println(p1.getFileName());
+    }
+}
