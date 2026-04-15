d7c47c21e718cd59dbc7ae850a53d0dfb1bdd020
Leonardo Santana da Silva <lsantanasilva98@gmail.com>
2026-04-15 15:23:55 -0300

    adiciona exemplos de classes internas e locais

--- src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest01.java
@@ -1,4 +1,19 @@
 package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
 
 public class OuterClassesTest01 {
+    private String name = "Gon";
+
+        class innerClass {
+
+            public void printAttribute() {
+                System.out.println(name);
+            }
+        }
+
+    static void main() {
+        OuterClassesTest01 outerClasses = new OuterClassesTest01();
+        innerClass innerClass = outerClasses.new innerClass();
+
+        innerClass.printAttribute();
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest02.java
+++ src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest02.java
@@ -1,4 +1,25 @@
 package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
 
 public class OuterClassesTest02 {
+    private String name = "Killua";
+
+    void print(final String param) {
+        final String lastName = "Zoldik";
+        class LocalClass {
+
+            public void localPrint() {
+                System.out.println(param);
+                System.out.println(name + " " + lastName);
+            }
+
+        }
+        new LocalClass().localPrint();
+    }
+
+    static void main() {
+
+        OuterClassesTest02 outer = new OuterClassesTest02();
+        outer.print("oi");
+
+    }
 }
